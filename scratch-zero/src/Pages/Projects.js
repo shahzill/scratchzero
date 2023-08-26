05070978d90b67817401eeb7d635dd83d1a40447
Shahzill Naveed <101993148+shahzill@users.noreply.github.com>
2023-08-25 20:46:09 -0600

    added the horizontal feature partially

--- scratch-zero/src/Pages/Projects.js
+++ scratch-zero/src/Pages/Projects.js
@@ -7,6 +7,9 @@ import { Helmet } from "react-helmet";
 import ReactTyped from "react-typed";
 import Particles from '../components/Particles';
 import { styles } from '../Styles/Projects.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
 
 
 import {BrowserRouter as Router, Switch, Route, Link, useNavigate, useParams} from 'react-router-dom';
@@ -14,6 +17,40 @@ import {BrowserRouter as Router, Switch, Route, Link, useNavigate, useParams} fr
 import { useAuth } from "./auth";
  
 function Projects() {
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
 
     return(
         <>
@@ -41,34 +78,94 @@ function Projects() {
                 <h1><span class="gradient-underline">SOFTWARE DEVELOPMENT</span>  </h1> 
             </div>
             <div className="Projects">
+                
             <div className="cards">
-    <div class="container">
-    <div class="card">
-    <div class="Pfront Pfront2"></div>
-      <div class="Pback Pback2">
-        <p>Additional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the card</p>
-        <a href="https://reservim.com/" target="_blank">
-      <button className="projectButton">
-        View Project
-      </button></a>
+            <div className="media-scroller">
+                <div class="Pcontainer">
+                <div class="card">
+                <div class="Pfront Pfront2"></div>
+                <div class="Pback Pback2">
+                    <p>Additional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the card</p>
+                    <a href="https://reservim.com/" target="_blank">
+                <button className="projectButton">
+                    View Project
+                </button></a>
 
-      </div>
-    </div>
-  </div>
-  <div class="container">
-    <div class="card">
-      <div class="Pfront Pfront2"></div>
-      <div class="Pback Pback2">
-        <p>Additional info on the back of the card</p>
-        <a href="https://doctorfindy.com/" target="_blank">
-      <button className="projectButton">
-        View Project
-      </button></a>
-      </div>
-    </div>
-  </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="Pcontainer">
+                <div class="card">
+                <div class="Pfront Pfront2"></div>
+                <div class="Pback Pback2">
+                    <p>Additional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the card</p>
+                    <a href="https://reservim.com/" target="_blank">
+                <button className="projectButton">
+                    View Project
+                </button></a>
 
-  </div>            </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="Pcontainer">
+                <div class="card">
+                <div class="Pfront Pfront2"></div>
+                <div class="Pback Pback2">
+                    <p>Additional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the card</p>
+                    <a href="https://reservim.com/" target="_blank">
+                <button className="projectButton">
+                    View Project
+                </button></a>
+
+                    </div>
+                    </div>
+                </div>
+                <div class="Pcontainer">
+                <div class="card">
+                <div class="Pfront Pfront2"></div>
+                <div class="Pback Pback2">
+                    <p>Additional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the card</p>
+                    <a href="https://reservim.com/" target="_blank">
+                <button className="projectButton">
+                    View Project
+                </button></a>
+
+                    </div>
+                    </div>
+                </div>
+
+            <div class="Pcontainer">
+            <div class="card">
+            <div class="Pfront Pfront2"></div>
+            <div class="Pback Pback2">
+                <p>Additional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the card</p>
+                <a href="https://reservim.com/" target="_blank">
+            <button className="projectButton">
+                View Project
+            </button></a>
+
+                </div>
+                </div>
+            </div> 
+
+            <div class="Pcontainer">
+            <div class="card">
+            <div class="Pfront Pfront2"></div>
+            <div class="Pback Pback2">
+                <p>Additional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the cardAdditional info on the back of the card</p>
+                <a href="https://reservim.com/" target="_blank">
+            <button className="projectButton">
+                View Project
+            </button></a>
+
+                </div>
+                </div>
+            </div> 
+            
+           
+            </div>
+            </div>
+            </div>
             <div className="Pquote1">
             <p>OUR TEAM OF SKILLED DEVELOPER, DESIGNERS, AND ENGINEERS COLLABORATE TO CRAFT SOLUTIONS THAT DRIVE INNVOVATION AND ELEVATE BUSINESS</p>
             </div>
